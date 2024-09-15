e5adef17a947b4913b0d7a15129cf51a34363375
tosh-m12 <tosh_m12@yahoo.co.jp>
2024-09-15 23:04:40 +0800

    課題006提出

---
+++ js/class-operation.js
@@ -0,0 +1,19 @@
+$(function() {
+    // $('#add').on('click', function(){
+    //     $('#target').addClass('sample');
+    // });
+    // $('#remove').on('click', function(){
+    //     $('#target').removeClass('sample');
+    // });
+    $('#toggle').on('click', function(){
+        $('#target').toggleClass('sample');
+    });
+    $('#has').on('click', function(){
+        let result = $('#target').hasClass('sample');
+        if(result) {
+            console.log('sampleクラスがあります');
+        } else {
+            console.log('sampleクラスがありません');
+        }
+    });
+});
